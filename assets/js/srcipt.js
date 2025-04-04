b6238f88be020387fd7a1895f0eaefb99e28671c
MesudQuluzade <quluzademesud6@gmail.com>
2025-04-04 16:48:03 +0400

    04.04.2025

--- assets/js/srcipt.js
+++ assets/js/srcipt.js
@@ -2,42 +2,45 @@ const menuToggle = document.getElementById("menu-toggle");
 const closeIcon = document.getElementById("close-icon");
 const mobileMenuContent = document.getElementById("mobile-menu-content");
 const mobileMenu = document.getElementById("mobile-menu");
-const dropdownButton = document.querySelector(".dropdown-menu");
+const dropdownButton = document.querySelector(".dropdown-menu button");
 const megaMenu = document.querySelector(".mega-menu");
 
 menuToggle.addEventListener("click", (event) => {
-    event.stopPropagation();
-    menuToggle.classList.add("hidden");
-    closeIcon.classList.remove("hidden");
-    mobileMenuContent.classList.remove("hidden");
+  event.stopPropagation();
+  menuToggle.classList.add("hidden");
+  closeIcon.classList.remove("hidden");
+  mobileMenuContent.classList.remove("hidden");
 });
 
 closeIcon.addEventListener("click", (event) => {
-    event.stopPropagation();
-    menuToggle.classList.remove("hidden");
-    closeIcon.classList.add("hidden");
-    mobileMenuContent.classList.add("hidden");
-});
-
-document.addEventListener("click", (event) => {
-    if (!mobileMenu.contains(event.target) && !mobileMenuContent.contains(event.target)) {
-        menuToggle.classList.remove("hidden");
-        closeIcon.classList.add("hidden");
-        mobileMenuContent.classList.add("hidden");
-    }
+  event.stopPropagation();
+  menuToggle.classList.remove("hidden");
+  closeIcon.classList.add("hidden");
+  mobileMenuContent.classList.add("hidden");
 });
 
 dropdownButton.addEventListener("click", (event) => {
-    event.stopPropagation();
-    megaMenu.classList.toggle("active");
+  event.stopPropagation();
+  megaMenu.classList.toggle("active");
 });
 
 document.addEventListener("click", (event) => {
-    if (!dropdownButton.contains(event.target) && !megaMenu.contains(event.target)) {
-        megaMenu.classList.remove("active");
-    }
-});
+  if (
+    !dropdownButton.contains(event.target) &&
+    !megaMenu.contains(event.target)
+  ) {
+    megaMenu.classList.remove("active");
+  }
 
+  if (
+    !menuToggle.contains(event.target) &&
+    !mobileMenuContent.contains(event.target)
+  ) {
+    menuToggle.classList.remove("hidden");
+    closeIcon.classList.add("hidden");
+    mobileMenuContent.classList.add("hidden");
+  }
+});
 
 
 $(document).ready(function () {
